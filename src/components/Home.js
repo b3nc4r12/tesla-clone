503f1bc543d4d487777baa8317beaa5f5dda3dd9
Ben Carter <benjcarter06@gmail.com>
2021-05-15 16:59:39 -0300

    ADD: Home Page Sections

---
+++ src/components/Home.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+    return (
+        <Container>
+            <Section
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                image="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                image="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                image="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                image="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Solar and Powerwall"
+                description="Power Everything"
+                image="solar-panel.jpg"
+                leftBtnText="Learn More"
+            />
+            <Section
+                title="Accesories"
+                description=""
+                image="accessories.jpg"
+                leftBtnText="Shop Now"
+            />
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    height: 100vh;
+`;
+
+export default Home
